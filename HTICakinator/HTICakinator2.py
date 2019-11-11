e7f68f3dc44f0d12d31c6ed527e990e0983d9746
Kazuma Takahara <4269kzm@gmail.com>
2019-11-11 17:29:58 +0900

    コードの整理

--- HTICakinator/HTICakinator2.py
+++ HTICakinator/HTICakinator2.py
@@ -10,187 +10,141 @@ import heapq
 import socket
 
 THRESHOLD_ANS = 0.95 # 確率しきい値
-MAX_QUESTIONS = 10 # 最大質問数
+MAX_QUESTIONS = 10 # 最大出題質問数
 NUM_CHOICE = 1 # ランダム上位質問候補数
+
 
 class HTICakinator:
     def __init__(self, database_path):
-        self.database = cl.OrderedDict()    # 辞書型の順序固定
-
-        #json形式で質問データベースを書き込み
-        #fw = open('database.json', 'w')
-        #json.dump(self.database, fw, indent=4)
-        #print(self.database)
+        self._database = cl.OrderedDict()    # 辞書型の順序固定
 
         #json形式の質問データベースを読み込み
-        fr = open(database_path, 'r', encoding="utf-8_sig")
-        self.database = json.load(fr)
-        #print(self.database)
-
-        self.p = {};
-        #init p
-        for key in self.database.keys():
-            self.p[key] = 1.0 / len(self.database.keys())
+        with open(database_path, 'r', encoding="utf-8_sig") as fr:
+            self._database = json.load(fr)
+            self._assert_valid_database(self._database)
         
-        self.q_list = []#[102123, 24511, 53355]]
-        self.a_list = []
-        self.threshold_ans = THRESHOLD_ANS
-        self.max_questions = MAX_QUESTIONS
+        self._p = {}
+        #init p
+        for key in self._database.keys():
+            self._p[key] = 1.0 / len(self._database.keys())
+        
+        self._q_list = []
+        self._a_list = []
+        self._threshold_ans = THRESHOLD_ANS
+        self._max_questions = MAX_QUESTIONS
 
         #init diseases
-        self.diseases = self.database.keys()
+        self._diseases = self._database.keys()
 
         #init candidate
-        self.q_candidates = []
-        for key in self.database.keys():
-            qs = self.database[key]
+        self._q_candidates = []
+        for key in self._database.keys():
+            qs = self._database[key]
             for q in qs.keys():
-                self.q_candidates.append(q)
+                self._q_candidates.append(q)
         
-        self.q_candidates = list(set(self.q_candidates))
+        self._q_candidates = list(set(self._q_candidates))
+        
+        # init first question
+        self._q_list.append(self._decideQ())
+        
+    def _assert_valid_database(self, database):
+        all_questions = {q for qs in database.values() for q in qs}
+        for q in all_questions:
+            for questions in database.values():
+                assert q in questions
 
-    def isLastQuestion(self):
-        (disease, est) = self.getBestEstimate()
-        if(est > self.threshold_ans):   # 確率がしきい値以上
-            return True
-        elif(len(self.q_list) >= self.max_questions):   # 最大質問数以上
-            return True
-        else:
-            return False
-
-    def calculateE(self, ps):
+    def _calculateE(self, ps):
         entropy = 0
-        for disease in self.diseases:
+        for disease in self._diseases:
             entropy += - ps[disease] * math.log2(ps[disease])
         return entropy
 
 
-    def calculateGainE(self, current_entropy, q):
+    def _calculateGainE(self, current_entropy, q):
 
         #first calc if a = 0
         ## calc weight for a = 0
         w_0 = 0
         ### p(disease) * p(ans = 0 | disease)
-        for disease in self.diseases:
-            w_0 += self.p[disease] * (1.0 * self.database[disease][q][0]/(self.database[disease][q][0]+self.database[disease][q][1]))
+        for disease in self._diseases:
+            w_0 += self._p[disease] * (1.0 * self._database[disease][q][0]/(self._database[disease][q][0]+self._database[disease][q][1]))
         
         ### calc_p after know
-        p_0 = self.updateP(self.p, q, 0)
-        e_0 = self.calculateE(p_0)
+        p_0 = self._newP(self._p, q, 0)
+        e_0 = self._calculateE(p_0)
 
         #first calc if a = 1
         ## calc weight for a = 1
         w_1 = 1 - w_0
 
-        p_1 = self.updateP(self.p, q, 1)
-        e_1 = self.calculateE(p_1)
+        p_1 = self._newP(self._p, q, 1)
+        e_1 = self._calculateE(p_1)
 
         # p(q_ans = 0) * E(q_ans=0) + p(q_ans = 1) * E(q_ans=1)
         return current_entropy - (w_0 * e_0 + w_1 * e_1)
 
-
-    def showQ(self, q):
-        print(" current p :  "+str(self.p))
-        print (str(len(self.q_list)+1) + ":" + q + " (Y / y / Yes / yes)")
-
-    def isNeedContinueQ(self):
-        if(self.isLastQuestion()):
-            return False
-        return True
-    
-    def decideQ(self):
-    #search best question
-        #max_e = float("-inf")
-        #max_e_q = ""
+    def _decideQ(self):
+        #search best question
         e_q_list = []
 
-        cur_entropy = self.calculateE(self.p)
+        cur_entropy = self._calculateE(self._p)
 
-        for q_candidate in self.q_candidates:
+        for q_candidate in self._q_candidates:
             # 既出質問は飛ばす
-            if q_candidate in self.q_list:
+            if q_candidate in self._q_list:
                 continue
 
-            e = self.calculateGainE(cur_entropy, q_candidate)
+            e = self._calculateGainE(cur_entropy, q_candidate)
             e_q_list.append((e, q_candidate))
-
-            # 最大エントロピーの質問を選択
-            #if max_e < e:
-            #    max_e = e
-            #    max_e_q = q_candidate
 
         max_nth_e_q = np.array(heapq.nlargest(NUM_CHOICE, e_q_list))	# エントロピー上位n個の質問抽出
         print(max_nth_e_q)
         nth_p = max_nth_e_q[:,0].astype(np.float32) / np.sum(max_nth_e_q[:,0].astype(np.float32))
         return np.random.choice(max_nth_e_q[:,1], p = nth_p)	# エントロピーを選択確率として質問をランダム選択
 
-        # return max_e_q
-
-    def updateP(self, p, q, a):
+    def _newP(self, p, q, a):
         new_p = {}
 
         base_p = 0
-        for disease in self.diseases:
-            base_p += (self.database[disease][q][a]*1.0/(self.database[disease][q][0] + self.database[disease][q][1])) * p[disease]
+        for disease in self._diseases:
+            base_p += (self._database[disease][q][a]*1.0/(self._database[disease][q][0] + self._database[disease][q][1])) * p[disease]
 
-        for disease in self.diseases:
+        for disease in self._diseases:
             # p(c | qs, as, q, a) = p(a | c, q) | p(c | qs, as)
-            new_p[disease] = 1.0 / base_p *(self.database[disease][q][a]*1.0/(self.database[disease][q][0] + self.database[disease][q][1])) * p[disease]
+            new_p[disease] = 1.0 / base_p *(self._database[disease][q][a]*1.0/(self._database[disease][q][0] + self._database[disease][q][1])) * p[disease]
 
         return new_p
         
-    def updateDatabase(self):
+    def question(self):
+        if self.finished():
+            return None
+        else:
+            return self._q_list[-1]
+    
+    def answer(self, a):
+        assert not self.finished(), 'already finished'
+        
+        a = {'yes': 0, 'no': 1}[a]
+        self._p = self._newP(self._p, self._q_list[-1], a)
+        self._a_list.append(a)
+        self._q_list.append(self._decideQ())
+    
+    def finished(self):
         (disease, est) = self.getBestEstimate()
-        for q, a in zip(self.q_list, self.a_list):
-            self.database[disease][q][a] += 1
-
+        if(est > self._threshold_ans):   # 確率がしきい値を超えている
+            return True
+        elif(len(self._q_list) > self._max_questions):   # 最大出題質問数を超えている
+            return True
+        else:
+            return False
+        
     def getBestEstimate(self):
         maxEstimate = 0
         maxEstimateDisease = ""
-        for disease in self.diseases:
-            if maxEstimate < self.p[disease]:
-                maxEstimate = self.p[disease]
+        for disease in self._diseases:
+            if maxEstimate < self._p[disease]:
+                maxEstimate = self._p[disease]
                 maxEstimateDisease = disease
 
         return (maxEstimateDisease, maxEstimate)
-
-    def showAndAskAnswer(self):
-        (disease, est) = self.getBestEstimate()
-        print( disease + " : " + str(est) + " (Y / y / Yes / yes)")
-        # クライアントに質問送信
-        self.conn.sendall((disease + " : " + str(est) + " (Y / y / Yes / yes)").encode('utf-8'))
-
-        last_ans = self.answer()
-        return last_ans
-
-    def answer(self, input_text):
-        if input_text == "Y" or input_text == "y" or input_text == "Yes" or  input_text == "yes":
-            return 0
-        else:
-            return 1
-
-    def main(self):
-        while(self.isNeedContinueQ()):
-            q = self.decideQ()
-            self.showQ(q)
-            self.q_list.append(q)
-
-            a = self.answer()
-            self.a_list.append(a)
-            self.p = self.updateP(self.p, q, a)
-
-        print(" result p :  "+str(self.p))
-        result = self.showAndAskAnswer()
-        #if result == True:
-        #    self.updateDatabase()
-
-
-if __name__ == "__main__":
-    parser = argparse.ArgumentParser(
-        description='HTIC akinator')
-    parser.add_argument('database_path', help='Path to database.json')
-    args = parser.parse_args()
-   
-    hticakinator = HTICakinator(args.database_path)
-    hticakinator.main()
-    
--- HTICakinator/app.py
+++ HTICakinator/app.py
@@ -2,93 +2,78 @@ from flask import Flask, render_template, request, session, redirect, jsonify
 import sqlite3
 import pickle
 from uuid import uuid4
+from datetime import datetime, timedelta
 
 from sqlalchemy import create_engine, Column, Integer
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-import HTICakinator
 import argparse
 import socket
 import threading
 
 from HTICakinator2 import HTICakinator
 
-MESSAGE = ''
-
 app = Flask(__name__)
 app.config['TEMPLATES_AUTO_RELOAD'] = True
-app.secret_key = 'change-me'
+app.secret_key = 'change me!!'
 
-akinator_from_user = {}
+_akinator_from_user = {}
+_AKINATOR_LIFESPAN = timedelta(hours=1)
+
+
+def delete_not_used_akinators():
+    for user, akinator in list(_akinator_from_user.items()):
+        if akinator.created_at + _AKINATOR_LIFESPAN < datetime.now():
+            del _akinator_from_user[user]
+
+
+def require_akinator(create=False):
+    user = session['user'] = session.get('user', uuid4())
+    if create and user not in _akinator_from_user:
+        _akinator_from_user[user] = HTICakinator('database_dummy1011.json')
+        _akinator_from_user[user].created_at = datetime.now()
+        
+    delete_not_used_akinators()
+    
+    return _akinator_from_user.get(user)
+
+
+def delete_akinator():
+    if 'user' in session and session['user'] in _akinator_from_user:
+        del _akinator_from_user[session['user']]
+    
 
 @app.route('/', methods=['GET'])
-def index():
-    user = session['user'] = session.get('user', uuid4())
-    if user not in akinator_from_user:
-        akinator_from_user[user] = HTICakinator('database_dummy1011.json')
-        akinator_from_user[user].q = None
-    akinator = akinator_from_user[user]
-
-    if akinator.isNeedContinueQ():
-        if akinator.q is None:
-            akinator.q = akinator.decideQ()
-            akinator.q_list.append(akinator.q)
-
-        return render_template('index2.html',
-                               title='動悸チェッカーサンプル',
-                               message='あなたの動悸症状に関する質問に答えてください ({})'.format(akinator.a_list),
-                               question=akinator.q)
-    else:
+def question():
+    akinator = require_akinator(create=True)
+    
+    if akinator.finished():
         (disease, est) = akinator.getBestEstimate()
         return render_template('index2.html',
                                title='動悸チェッカーサンプル',
-                               message='あなたの動悸症状に関する質問に答えてください ({})'.format(akinator.a_list),
-                               question='{}: {}'.format(disease, est))
+                               message='チェックが完了しました',
+                               question='{}: {:.2%}'.format(disease, est))
+    else:
+        return render_template('index2.html',
+                               title='動悸チェッカーサンプル',
+                               message='あなたの動悸症状に関する質問に答えてください',
+                               question=akinator.question())
 
 
 @app.route('/answer/<yes_or_no>', methods=['GET'])
 def answer(yes_or_no):
-    if 'user' not in session or session['user'] not in akinator_from_user:
-        return redirect('/')
-    
-    akinator = akinator_from_user[session['user']]
-    
-    if akinator.isNeedContinueQ():
-        a = 0 if yes_or_no == 'yes' else 1
-        akinator.a_list.append(a)
-        akinator.p = akinator.updateP(akinator.p, akinator.q, a)
-        akinator.q = None
-    
+    akinator = require_akinator(create=False)
+    if akinator is not None and not akinator.finished():
+        akinator.answer(yes_or_no)
     return redirect('/')
     
 
 @app.route('/reset', methods=['GET'])
 def reset():
-    if 'user' not in session or session['user'] not in akinator_from_user:
-        return redirect('/')
-    del akinator_from_user[session['user']]
+    delete_akinator()
     return redirect('/')
 
-def socketConnect():
-    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-        # サーバを指定
-        s.connect(('127.0.0.1', 50007))
-        # s.connect((args.ip, args.port))
-
-        while True:
-            # ネットワークのバッファサイズは1024。サーバからの質問文字列を取得する
-            message = s.recv(1024).decode('utf-8')
-            if message == "END":
-                break
-            # 質問表示
-            print(message)
-
-            # 回答入力待ち
-            input_text = input()
-
-            # サーバに回答を送る
-            s.sendall(input_text.encode('utf-8'))
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(
@@ -96,9 +81,6 @@ if __name__ == '__main__':
     parser.add_argument('--ip', default='127.0.0.1', help='Server IP address')
     parser.add_argument('--port', default=50007, type=int, help='Server connection port')
     args = parser.parse_args()
-
-    # thread_1 = threading.Thread(target=socketConnect)
-    # thread_1.start()
 
     app.debug = True
     app.run(host='localhost')
